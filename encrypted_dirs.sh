46d9ae1580eaea724e12827b900fff84dd92111b
arcaneJesus <elijahz0409@gmail.com>
2022-11-07 04:37:17 -0500

    Implemented closedir() function

--- encrypted_dirs.sh
+++ encrypted_dirs.sh
@@ -1,9 +1,6 @@
 # Encypted directories
-source ./kv-bash
+source ~/.shellconfig/libs/kv-bash
 encryptdir () {
-  if ![[ -e ~/.links.csv ]]; then
-    touch ~/.links.csv
-  fi
   # encrypt and add to links
   if ![[ -e $1]]; then
     mkdir $1
@@ -12,11 +9,23 @@ encryptdir () {
   userfacing=$(readlink -f $2)
   encfs $secret $userfacing
   fusermount -u $2
-  echo "$secret, $userfacing" >> ~/.links.csv
+  kvset $secret $userfacing
 }
 
 opendir () {
   if [[ -e $1 ]]; then
-    
+    dir=$(readlink -f $1)
+    secret=$(kvget $dir)
+    encfs $secret $dir
+  else
+    echo "Dir not found"
   fi
 }
+
+closedir() {
+  if [[ -e $1 ]] && [[ -z $(kvget $(readlink -f $1)) ]]; then
+    fusermount -u $1
+  else
+    echo "error"
+  fi
+
